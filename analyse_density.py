616a996a511fd3c5d49dc4a1626a47237cf0fbff
marcomusy <marco.musy@gmail.com>
2023-02-28 00:48:33 +0100

    fix zoom

--- analyse_density.py
+++ analyse_density.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#
 import sys
 from vedo import settings, np
 from vedo.pyplot import plot, fit
@@ -7,7 +7,7 @@ from point_plotter import Plotter, PointPlotter
 
 # Data ###################################
 namein  = sys.argv[1]
-nameout = namein.replace('.tif','') + "_data.csv"
+nameout = namein.replace('.tif','').replace('.png','') + "_data.csv"
 radius = 100  # pixels
 
 plt1 = PointPlotter(namein, nameout, init=nameout)
@@ -22,11 +22,12 @@ intensities = plt1.get_intensities()
 coeff = np.corrcoef(dens_array, intensities)[0][1]
 
 fig = plot(
-    dens_array, intensities,
-    lw=0,         # do not join points with lines
+    dens_array, 
+    intensities,
     title=f"Corr. coeff: {coeff: .3f}",
     xtitle=f"Density in {radius} pixels radius",
     ytitle="Sox9 intensity",
+    lw=0,         # do not join points with lines
     marker="*",   # marker style
     mc="red4",    # marker color
     aspect=1/1,   # aspect ratio
@@ -34,7 +35,7 @@ fig = plot(
 fig += fit([dens_array, intensities])
 
 plt2 = Plotter(N=2, sharecam=False)
-plt2.at(0).show(namein, pts, dens, plt1.pic.alpha(0.2), zoom='tigh')
+plt2.at(0).show(namein, pts, dens, plt1.pic.alpha(0.2), zoom='tight')
 plt2.at(1).show(fig, zoom='tight')
 plt2.screenshot(nameout.replace("data.csv","screenshot.png"))
 plt2.interactive().close()
