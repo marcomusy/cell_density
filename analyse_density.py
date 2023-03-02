b5f21b3be529318d88e29ede9e3ee645ee65f5e7
marcomusy <marco.musy@gmail.com>
2023-03-02 19:21:43 +0100

    various improvements

--- analyse_density.py
+++ analyse_density.py
@@ -6,37 +6,42 @@ from vedo.pyplot import plot, fit
 from point_plotter import Plotter, PointPlotter
 
 # Data ###################################
-namein  = sys.argv[1]
-nameout = namein.replace('.tif','').replace('.png','') + "_data.csv"
+namein = sys.argv[1]
+nameout = namein.replace(".tif", "").replace(".png", "") + "_data.csv"
 radius = 100  # pixels
 
 plt1 = PointPlotter(namein, nameout, init=nameout)
-plt1.point_color = 'red5'
-plt1.show(zoom='tight', mode='image').close()
+plt1.point_color = "red5"
+plt1.point_size = 8
+plt1.show(zoom="tightest", mode="image").close()
 
 # Analysis ################################
 settings.enable_default_keyboard_callbacks = True
 
 pts, dens, dens_array = plt1.compute_density(radius)
-intensities = plt1.get_intensities()
+# intensities = plt1.get_intensities()         # TODO get intensities of GFP
+intensities = np.random.rand(len(dens_array))  # TODO random values for now
 coeff = np.corrcoef(dens_array, intensities)[0][1]
 
 fig = plot(
-    dens_array, 
+    dens_array,
     intensities,
-    title=f"Corr. coeff: {coeff: .3f}",
+    title=f"Corr. coeff: {coeff: .3f} (n={len(dens_array)})",
     xtitle=f"Density in {radius} pixels radius",
-    ytitle="Sox9 intensity",
-    lw=0,         # do not join points with lines
-    marker="*",   # marker style
-    mc="red4",    # marker color
-    aspect=1/1,   # aspect ratio
+    ytitle="Intensity",
+    lw=0,          # do not join points with lines
+    marker="*",    # marker style
+    mc="red4",     # marker color
+    aspect=1 / 1,  # aspect ratio
 )
-fig += fit([dens_array, intensities])
+
+try:
+    fig += fit([dens_array, intensities])
+except:
+    pass
 
 plt2 = Plotter(N=2, sharecam=False)
-plt2.at(0).show(namein, pts, dens, plt1.pic.alpha(0.2), zoom='tight')
-plt2.at(1).show(fig, zoom='tight')
-plt2.screenshot(nameout.replace("data.csv","screenshot.png"))
+plt2.at(0).show(namein[-30:], pts, dens, dens.box(), zoom=1.05)
+plt2.at(1).show(fig, zoom="tight", mode="image")
+plt2.screenshot(nameout.replace("data.csv", "screenshot.png"))
 plt2.interactive().close()
-
--- point_plotter.py
+++ point_plotter.py
@@ -1,5 +1,5 @@
 import numpy as np
-from vedo import printc, precision, settings, probe_points
+from vedo import printc, settings, probe_points
 from vedo import Plotter, Picture, Points, Text2D
 
 settings.use_parallel_projection = True
@@ -7,43 +7,45 @@ settings.enable_default_keyboard_callbacks = False
 
 txt = """Click to add a point
 Right-click to remove it
-Press c to clear points
-Press w to write points"""
+Press w to write points
+Press q to continue"""
 instructions = Text2D(
-    txt, pos="bottom-left", font="Quikhand", c="white", bg="green4", alpha=0.5
+    txt, pos="bottom-left", font="Quikhand", c="white", bg="green4", alpha=0.75
 )
 
 #############################
 class PointPlotter(Plotter):
     def __init__(self, filename, nameout, **kwargs):
-        initfile = kwargs.pop("init", None)
+
+        kwargs["title"] = "Cell Density Tool"
         kwargs["bg"] = "black"
+        initfile = kwargs.pop("init", None)
+        
         super().__init__(**kwargs)
 
-        self.point_size = 15
-        self.point_color = "red8"
+        self.point_size = 8
+        self.point_color = "red7"
         self.filename = filename
         self.nameout = nameout
-        self.pic = Picture(filename).flip("y").smooth(1)
-        #self.pic_copy = self.pic.clone() #make a copy
+        self.pic = Picture(filename).flip("y")#.smooth(1)
 
         self.cpoints = []
         self.points = None
         if initfile is not None:
             try:
-                self.cpoints = np.loadtxt(initfile, delimiter=",").tolist()
-                self.points = Points(np.array(self.cpoints)[:, (0, 1)])
-                self.points.ps(self.point_size).c(self.point_color)
+                self.cpoints = np.loadtxt(initfile, delimiter=",")[:,(0,1)].tolist()
+                self.points = Points(self.cpoints).ps(self.point_size).c(self.point_color)
             except FileNotFoundError:
                 printc("No init file", initfile, "continue.", c="y")
-        self += [self.pic, self.points, instructions, self.filename]
+        
+        tx = Text2D(self.filename, font="Calco", s=0.9, bg='yellow3', alpha=0.75)
+        self += [self.pic, self.points, instructions, tx]
 
         self.add_callback("key press", self.on_key_press)
         self.add_callback("left mouse click", self.on_left_click)
         self.add_callback("right mouse click", self.on_right_click)
 
     def on_left_click(self, evt):
-        col = self.color_picker(evt.picked2d)
         if not evt.actor:
             return
         if evt.actor.name == "points":
@@ -51,36 +53,27 @@ class PointPlotter(Plotter):
             pid = self.points.closest_point(evt.picked3d, return_point_id=True)
             self.cpoints.pop(pid)
             self.update()
-            printc("Deleted point id", pid, c="r")
             return
-        p = evt.picked3d
-        p = [p[0], p[1], np.mean(col)]
-        self.cpoints.append(p)
+        self.cpoints.append(evt.picked3d[:2])
         self.update()
-        printc("Added point:", precision(p[:2], 4), col, c="g")
+        # col = self.color_picker(evt.picked2d) # TODO get color of GFP
 
     def on_right_click(self, evt):
         if evt.actor and len(self.cpoints) > 0:
             self.cpoints.pop()  # pop removes from the list the last pt
             self.update()
-            printc("Deleted last point", c="r")
 
     def on_key_press(self, evt):
         if "q" in evt.keypress.lower():
             out = np.round(self.cpoints).astype(int)
-            np.savetxt(".tmp.csv", out, delimiter=",") # one can forget to save
+            # because one can forget to save the points
+            np.savetxt("tmp_saved_coords.csv", out, fmt='%i', delimiter=",")
             self.close()
             return
-        elif evt.keypress == "c":
-            self.cpoints = []
-            self.remove(self.points).render()
-            printc("==== Cleared all points ====", c="r", invert=True)
         elif evt.keypress == "w":
-            printc(f"==== Saved file {self.nameout} ====", c="b", invert=True)
-            printc("   x   y   I    ", c="b", invert=True)
             out = np.round(self.cpoints).astype(int)
-            printc(out, c="b")
-            np.savetxt(self.nameout, out, delimiter=",")
+            np.savetxt(self.nameout, out, fmt='%i', delimiter=",")
+            printc(f"Saved {len(out)} cells in file {self.nameout}", c="b", invert=True)
 
     def update(self):
         self.remove(self.points)  # remove old points
@@ -96,13 +89,13 @@ class PointPlotter(Plotter):
             exit()
         return np.round(self.cpoints).astype(int)[:, (0, 1)]
 
-    def get_intensities(self):
-        return np.round(self.cpoints).astype(int)[:, 2]
+    # def get_intensities(self):  # TODO get intensities of GFP
+    #     return np.round(self.cpoints).astype(int)[:, 2]
 
     def compute_density(self, radius):
         cc = self.get_coordinates()
         pts = Points(cc)
-        vol = pts.density(radius).c("Paired_r")  # returns a Volume
+        vol = pts.density(radius).c("YlOrRd").alpha(1)  # returns a Volume
         vol.add_scalarbar3d(title=f"Density (counts in r_search ={radius})", c="k")
         mpts = probe_points(vol, pts).ps(5).c("black")
         return mpts, vol, mpts.pointdata["ImageScalars"]
